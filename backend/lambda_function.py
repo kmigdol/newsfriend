72d6e78f561816f02c6efd598434503a3d13f29f
Kayleigh Migdol <kayleigh@migdol.net>
2023-06-27 21:32:41 -0700

    search news api again

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -14,6 +14,8 @@ GOOGLE_URL = "https://www.googleapis.com/customsearch/v1"
 BING_KEY = os.environ["BING_KEY"]
 BING_URL = "https://api.bing.microsoft.com/v7.0/search"
 
+NEWSDATA_KEY = os.environ["NEWSDATA_KEY"]
+NEWSDATA_URL = "https://newsdata.io/api/1/news"
 
 ARTICLE_COUNT = 5
 
@@ -73,8 +75,17 @@ def _get_items_bing(query: str):
     items = res.json()["webPages"]["value"]
     return items
 
+def _get_items_newsdata(query: str):
+    params = {
+        "apikey": NEWSDATA_KEY,
+        "q": query
+    }
+    res = requests.get(NEWSDATA_URL, params)
+    return res.json()
+
 def get_articles_google(query: str) -> List[Article]:
     items = _get_items_google(query)
+    print(items)
     articles = [{"title": item["title"], "url": item["link"]} for item in items]
     return articles
 
@@ -83,13 +94,19 @@ def get_articles_bing(query: str) -> List[Article]:
     articles = [{"title": item["name"], "url": item["url"]} for item in items]
     return articles
 
+def get_articles_newsdata(query: str) -> List[Article]:
+    items = _get_items_newsdata(query)
+    articles = [{"title": item["title"], "url": item["link"]} for item in items]
+    return articles
+
 def run_articles_lambda(event):
     if "body" not in event:
         return {
             'statusCode': 200
         }
 
-    body = json.loads(event["body"])
+    # body = json.loads(event["body"])
+    body = event["body"]
     assert "title" in body, "missing title"
     article = body["title"]
     query = get_search_query(article)
@@ -139,11 +156,9 @@ def lambda_handler(event, context):
         } 
 
 if __name__ == "__main__":
-    event_body = {
-        "article_url": 'http://www.bbc.com/news/business-43298897'
-    }
-    res = get_search_query(event_body)
-    breakpoint()
+    query = "Supreme Court Updates"
+    res = _get_items_newsdata(query)
 
+    breakpoint()
     print(res)
 
